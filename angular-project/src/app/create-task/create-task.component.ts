e32a610e14be551dd3486abc09ff276ddac1e54d
GulityCrown <cupalukas2@gmail.com>
2025-03-05 22:17:30 +0100

    extractovanie textu z obrázka

--- angular-project/src/app/create-task/create-task.component.ts
+++ angular-project/src/app/create-task/create-task.component.ts
@@ -1,15 +1,19 @@
 import { Component, inject } from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormControl, FormGroup, FormsModule, NgModel, ReactiveFormsModule, Validators} from '@angular/forms';
 import { TaskDTO } from '../task-list/TaskDTO';
 import { TaskService } from 'src/services/task.service';
-import { min, Subject, takeUntil } from 'rxjs';
+import { buffer, min, Subject, takeUntil } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Router, RouterLink } from '@angular/router';
+import Tesseract from 'tesseract.js';
+import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-create-task',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink ],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule, FormsModule, MatIcon],
   templateUrl: './create-task.component.html',
   styleUrl: './create-task.component.css'
 })
@@ -40,5 +44,54 @@ submit(){
   .subscribe(() => this.router.navigate(['/taskList']));
 }
 
+extractedText: string = '';
+  loading: boolean = false;
+  searchText = '';
+  searchResult = '';
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
 
+      reader.onload = () => {
+        const imageData = reader.result as string;
+        this.performOCR(imageData);
+      };
+
+      reader.readAsDataURL(file); // Convert image to Base64 format
+    }
+  }
+
+  performOCR(imageData: string): void {
+    this.loading = true;
+
+    Tesseract.recognize(
+      imageData
+    )
+      .then(({ data: { text } }) => {
+        this.extractedText = this.removeAngleBrackets(text); // Extracted text
+        this.loading = false;
+      })
+      .catch((err) => {
+        console.error('OCR Error:', err);
+        this.loading = false;
+      });
+  }
+
+/*  search() {
+    if (this.extractedText.toUpperCase().includes(this.searchText.toUpperCase())) {
+      this.searchResult = 'Text "' + this.searchText + '"' + 'exists. Occurences :' + this.countNumberOfOccurences();
+    } else {
+      this.searchResult = "Not exist"
+    }
+  }
+
+  countNumberOfOccurences() {
+    const regex = new RegExp(this.searchText.toUpperCase(), "g");
+    return (this.extractedText.toUpperCase().match(regex) || []).length;    
+  }*/
+
+  removeAngleBrackets(text: string): string {
+    return text.replace(/[<>]/g, ' '); 
+  }
 }
